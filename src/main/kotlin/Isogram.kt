ac95d91a865e5be0b63c7d204ac60f1182e86c42
J.Tadebois <jpm.tadebois@gmail.com>
2021-03-15 01:10:03 +0400

    Add completed solution

--- src/main/kotlin/Isogram.kt
+++ src/main/kotlin/Isogram.kt
@@ -1,6 +1,11 @@
 object Isogram {
 
+    private const val alphabet = "abcdefghijklmnopqrstuvwxyz"
+
     fun isIsogram(input: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val lettersOnly = input.toLowerCase().filter { alphabet.contains(it) }
+        val lettersCount = lettersOnly.length
+        val uniqueLettersCount = lettersOnly.toSet().size
+        return lettersCount == uniqueLettersCount
     }
 }
